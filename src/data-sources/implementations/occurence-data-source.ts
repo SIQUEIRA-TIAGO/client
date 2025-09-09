14b8bd412e4664b8ca811b511d4dcd4a9ed6a4c1
Tiago Oliveira <oliveira.251203@gmail.com>
2025-09-09 18:48:18 -0300

    [ts] 🐛 fix: Returning requests to use axiosApiConnector

--- src/data-sources/implementations/occurence-data-source.ts
+++ src/data-sources/implementations/occurence-data-source.ts
@@ -2,32 +2,21 @@ import { IErrors } from '@/common/interfaces/errors';
 import { axiosApiConnector } from '../connectors/axios-api-connector';
 import { IOccurenceDataSource } from '../interfaces/ocurrence-data-source';
 import { globals } from '@/common/states/globals';
-import axios from 'axios';
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve(process.cwd(), '../.env') });
 
 export const ocurrenceDataSourceImpl: IOccurenceDataSource = () => ({
     postOcurrence: async (ocurrence) => {
         try {
-        const result = await axios.post(
-            `${process.env.CENTRAL_API_BASE_URL}/api/occurrence`,
-            {
-                client_id: ocurrence.client_id,
-                observer_id: ocurrence.observer_id,
-                ocurrence_url: ocurrence.ocurrence_url,
-                ocurrence_id: ocurrence.ocurrence_id,
-                ocurrenceJson: ocurrence.ocurrenceJson,
-                is_recovery: ocurrence.is_recovery
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${globals.access_token}`,
-                    'Content-Type': 'application/json',
-                }
-            }
-        );
+            const result = await axiosApiConnector({
+                method: 'post',
+                data: {
+                    client_id: ocurrence.client_id,
+                    observer_id: ocurrence.observer_id,
+                    ocurrence_url: ocurrence.ocurrence_url,
+                    ocurrence_id: ocurrence.ocurrence_id,
+                    ocurrenceJson: ocurrence.ocurrenceJson,
+                    is_recovery: ocurrence.is_recovery
+                },
+            });
 
             return result.data.payload;
         } catch (error) {
