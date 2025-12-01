9b0477af99086adf29aafaa8b6431bc3c9991578
Tiago Oliveira <oliveira.251203@gmail.com>
2025-12-01 10:49:31 -0300

    [ts] 🐛 fix: parseSQL acception false and 0 as condition value

--- src/common/helpers/parseSql.ts
+++ src/common/helpers/parseSql.ts
@@ -1,75 +1,33 @@
-import { ICondition, Column, ConditionType } from '@/entities/observer';
+import { ICondition, } from '@/entities/observer';
 import Twig from 'twig';
 
-export const getConditions = (sql: string): Record<string, ICondition> => {
-	const regex = /{{(.*?)}}/g;
-	const matches = sql.match(regex);
-	const result: Record<string, ICondition> = {};
+export const parseSQL = (code: string, conditions: Record<string, ICondition>) => {
+    const removeColumnsPattern = /columns\([^)]*\)/g;
+    const twigData = code
+        .replace(removeColumnsPattern, '')
+        .replace(/\{\{([^}]*)\}\}/g, (match, inner) => {
+            const cleaned = inner.replace(/\/\w+/g, '');
+            return `{{${cleaned.trim()}}}`;
+        });
 
-	matches?.forEach(match => {
-		const inside = match.slice(2, -2).trim();
-		const [ conditionWithOptionalType, ...params ] = inside.split(' ');
+    const twigRenderConditions = Object.keys(conditions).reduce((acc, condition) => {
+        const value = conditions[condition].value;
+        acc[condition] = value === null || value === undefined || value === ''
+            ? `{{${condition}}}`
+            : value;
 
-		const [ condition, type, ] = conditionWithOptionalType.split('/');
-		let columns: Column[] = [];
-		let required = true;
+        return acc;
+    }, {} as Record<string, string | boolean | number | null>);
 
-		if (params.length > 0 && params[0].startsWith('columns(')) {
-			const columnsStr = params.join(' ').slice(8, -1);
-			const columnsArray = columnsStr.split(',');
-			columns = columnsArray.map(col => {
-				const [ name, type = 'text', ] = col.split('/');
-				return {
-					name: name.replace(/\'/g, '').trim(),
-					type: type.replace(/\'/g, '').trim() as ConditionType,
-				};
-			});
-		}
+    const template = Twig.twig({ data: twigData, });
+    const twigRendered = template.render(twigRenderConditions);
 
-		const findBracketsRegex = /\[(.*?)\]/g;
-		const bracketMatchs = sql.match(findBracketsRegex);
-		bracketMatchs?.forEach(bracketMatch => {
-			if(bracketMatch.includes(match)){
-				required = false;
-			}
-		});
+    const output = twigRendered.replace(/\[(.*?)\]/gs, (match, content) => {
+        if (content.includes('{{')) {
+            return '';
+        }
+        return content;
+    });
 
-		result[condition] = {
-			type: columns.length > 0 ? 'sql' : type as ConditionType,
-			...(columns.length > 0 ? { columns, } : {}),
-			required,
-			value: null,
-		};
-	});
-
-	return result;
-};
-
-export const parseSQL = (code: string, conditions: Record<string, ICondition>) => {	
-	const removeColumnsPattern = /columns\([^)]*\)/g;
-	const removeWordsAfterSlash = /\/\w+/g;
-
-	const twigData = code
-		.replace(removeColumnsPattern, '')
-		.replace(/\{\{([^}]*)\}\}/g, (match, inner) => {
-			const cleaned = inner.replace(/\/\w+/g, '');
-			return `{{${cleaned.trim()}}}`;
-		});
-		
-	const twigRenderConditions = Object.keys(conditions).reduce((acc, condition) => {
-		acc[condition] = !conditions[condition].value ? `{{${condition}}}` : conditions[condition].value;
-		return acc;
-	}, {} as Record<string, string | boolean | number | null>);
-		
-	const template = Twig.twig({ data: twigData, });
-	const twigRendered = template.render(twigRenderConditions);
-	
-	const output = twigRendered.replace(/\[(.*?)\]/gs, (match, content) => {
-		if (content.includes('{{')) {
-			return '';
-		}
-		return content;
-	});
-		
-	return output;
+    return output;
 };
